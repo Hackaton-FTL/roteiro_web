524afa4e2d34b936aa7328174d9f35340999581e
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2025-09-03 19:20:48 +0100

    feat: implementa lógica de geração de roteiros com base em orçamento e atividades, atualiza template para exibir itinerários personalizados

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import db
+import db, random
 
 app = Flask(__name__)
 
@@ -7,10 +7,12 @@ app = Flask(__name__)
 def teardown_db(exception):
 	db.close_db(exception)
 
+
 @app.route("/createdb", methods=['GET'])
 def initdb():
 	db.init_db()
 	return "Banco de dados criado"
+
 
 @app.route("/", methods=['GET'])
 def home():
@@ -19,13 +21,36 @@ def home():
 	categorias = [dict(row) for row in rows ]
 	return render_template("index.html", categorias=categorias)
 
-# def generateItineraries():
+
+def generateItineraries(orcamento, actividades, dias):
+	roteiro = []
+	orcamento_restante = orcamento
+	horas_por_dia = 8
+	atividades_filtradas = [
+        a for a in actividades
+    ]
+	random.shuffle(atividades_filtradas)
+
+	for dia in range(1, dias + 1):
+		horas_disponiveis = horas_por_dia
+		actividades_dia = []
+		for atividade in atividades_filtradas:
+			duracao = atividade['duracao_horas']
+			custo = atividade['custo']
+
+			if (custo <= orcamento_restante and duracao <= horas_disponiveis ):
+				orcamento_restante -= custo
+				horas_disponiveis -= duracao
+				actividades_dia.append(atividade)
+				atividades_filtradas.remove(atividade)
+		roteiro.append({"dia": dia, "atividade": actividades_dia})
+
+	return roteiro
 
 
 @app.route("/roteiro", methods=['POST'])
 def getItineraries():
 	dias = int(request.form.get("dias"))
-	dias_em_horas = dias * 24
 	orcamento = float(request.form.get("orcamento"))
 	preferencias = request.form.getlist("preferencias")
 	preferencias_ids = [int(pref) for pref in preferencias ]
@@ -39,13 +64,10 @@ def getItineraries():
 		rows = query.execute("SELECT * FROM atividades").fetchall()
 
 	actividades = [dict(row) for row in rows ]
+	roteiro = generateItineraries(orcamento, actividades, dias)
 
-	return render_template("roteiro.html",
-		actividades=actividades,
-		dias=dias,
-		orcamento=orcamento,
-		preferencias=preferencias,
-		dias_em_horas=dias_em_horas)
+	return render_template("roteiro.html", roteiro=roteiro)
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
